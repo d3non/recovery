f7d40957bcdeb81008040eca380e3edeb8d4cbca
d3non <peps.valle@gmail.com>
2019-07-26 11:21:54 -0500

    Procfile modified

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
-
+//dbPassword = 'mongodb://heroku_zkt0016w'+ encodeURIComponent('heroku_zkt0016w') + '@ds037387.mlab.com:37387/heroku_zkt0016w?retryWrites=true';
+dbPassword = 'mongodb://heroku_zkt0016w:hoeab4sqr0n890dkvp9d5roav5@ds037387.mlab.com:37387/heroku_zkt0016w';
 module.exports = {
     mongoURI: dbPassword
 };
